54762f6b2bc59fcafeb84c36bd72d681ff97a183
Omar D. Ramirez <omardramirez2002@gmail.com>
2021-12-03 07:04:44 -0600

    This commit adds the mutations, queries, and outlines for the the typedefs.

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,7 +1,37 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
+type bookSchema{
+    authors: String
+    description: String!
+    bookId: String!
+    image: String
+    link: String
+    title: String!
+}
 
+type User{
+    username: String!
+    email: String!
+    password: String!
+    savedBooks:[bookSchema]
+}
+
+type Auth{
+    token: ID!
+    user: User
+}
+
+type Query{
+    getSingleUser: User
+}
+
+type Mutation{
+    createUser(username: String!, email: String!, password: String!): Auth
+    login(username: String!, email: String!): Auth
+    saveBook(UserId: ID!, bookId: ID!): User
+    deleteBook(UserId: ID!, bookId:ID!): User
+}
 `;
 
 module.exports = typeDefs;
